a2e3a2c9f8d7b2d22228514c2337b2283d1adb6a
fraggel <fraggelillo666@gmail.com>
2013-07-21 00:36:10 +0200

    Añadido tethering usb version 4, metidos más lanzamientos de los iptables en cuanto cambia algo de la red

---
+++ src/es/jiayu/tethering421jiayu/MainService.java
@@ -0,0 +1,12 @@
+package es.jiayu.tethering421jiayu;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class MainService extends Service {
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
